c2e3e937ec2320baeb6974803b0f3a8f4124393c
unknown <evorosh1@binghamton.edu>
2017-12-05 22:11:18 -0500

    fixed cookies

--- Cookies.py
+++ Cookies.py
@@ -1,6 +1,6 @@
 import pygame
-import Controller
 import Pacman
+import Maze
 
 
 class Cookies(pygame.sprite.Sprite):
@@ -13,17 +13,28 @@ class Cookies(pygame.sprite.Sprite):
 		self.type = ''
 		self.size = pygame.Surface.get_size()
 		self.pacman = Pacman
-                self.specialcookie
-	def get_postion(self):
-		pass
-                
+		self.specialcookie
+	def draw_postions(self):
+		self.specialcookie = [(10, 10), (10, 650), (300, 200), (400, 500)]
+		self.cookie = [(45, 660), (75, 140), (60, 320), (189, 575), (370, 250), (470, 470)]
+		self.allcoords = []
+		self.allcoords.add(self.cookie)
+		self.allcoords.add(self.specialcookie)
+		for i in range(len(self.specialcookie)):
+                        pygame.draw.circle(Surface,(63,63,63), i, 3)    
+												
 	def disappear(self):
-                eaten = False
-                while not eaten:
-                        if (self.pacman.get_position == self.cookie)
-						
-	def draw(self):
-	
+		eat = False
+		self.score = 0
+		while not eaten:
+                        if len(self.allcoords) == 0
+                                return self.score
+                        else:
+                                eaten = pygame.sprite.spritecollide(self.pacman, self.allcoords, True, collided)												 
+                                for eat in eaten:
+                                        self.score += 1
+                                        pygame.draw.circle(Surface,(0,0,0), eat, 3)
+                                return self.score
 	def getType(self):
 		return self.type
 	
